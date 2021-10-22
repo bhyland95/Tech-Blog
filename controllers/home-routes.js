5752e79f4e2452d139b72f0ebbb8b25d5add0a5f
Bhyland <blakehyland95@gmail.com>
2021-10-22 12:23:12 -0600

    hooked up homepage route. Added dummy CSS and dummy HTML

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { } = require('../models');
 
-
+router.get('/', (req, res) => {
+    res.render('homepage');
+});
 
 module.exports = router;
