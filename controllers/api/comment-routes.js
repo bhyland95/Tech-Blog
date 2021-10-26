a0be70468113599376331c0f874a20c10e3aafa0
bhyland <blakehyland95@gmail.com>
2021-10-25 22:28:20 -0600

    added api routes. small fixes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,7 +1,50 @@
 const router = require('express').Router();
-const {  } = require('../../models');
+const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+//gets all commetns
+router.get('/', (req, res) => {
+    Comment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
+//make a comment
+router.post('/', withAuth, (req, res) => {
+    // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.session.user_id,
+        post_id: req.body.post_id
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
+//delete a comment
+router.delete('/:id', withAuth, (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'No comment found with this id!' });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
